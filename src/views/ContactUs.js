abc289da3d305b2d91d0bcd03d6af29303577d29
baria aziz <bariaziz1996@gmail.com>
2020-12-16 10:07:31 +0500

    fix navbar issues in contact page, add content

--- src/views/ContactUs.js
+++ src/views/ContactUs.js
@@ -8,6 +8,17 @@ import PrimaryNavbar from "components/Navbars/PrimaryNavbar.js";
 import TransparentFooter from "components/Footers/TransparentFooter.js";
 
 function ContactUs() {
+  React.useEffect(() => {
+    document.body.classList.add("profile-page");
+    document.body.classList.add("sidebar-collapse");
+    document.documentElement.classList.remove("nav-open");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove("profile-page");
+      document.body.classList.remove("sidebar-collapse");
+    };
+  }, []);
   return (
     <>
       <PrimaryNavbar />
@@ -18,52 +29,50 @@ function ContactUs() {
               <Col className="ml-auto mr-auto text-center" md="8">
                 <h2 className="title">Get in touch</h2>
                 <h5 className="description font-weight-normal">
-                  We welcome queries from all our stakeholders and would like to
-                  hear from you
+                  Use the contact information below to contact us regarding any
+                  of our services. Our hard working team will get back to you as
+                  soon as possible.
                 </h5>
               </Col>
             </Row>
-            <div className="section">
-              <Row>
-                <Col md="3"></Col>
-                <Col md="6">
-                  <Card>
-                    <CardBody className="text-left">
-                      <p className="description text-left font-weight-normal">
-                        <i className="pr-1 fas fa-phone-square"></i>{" "}
-                        +923004548461
-                        <br></br>
-                        <i className="fas fa-fax"></i> 0092 51 2363409
-                        <br></br>
-                        <i className="pr-1 fas fa-envelope"></i>{" "}
-                        ibadsheryar@hotmail.com
-                        <br></br>
-                        <i className="pr-1 fas fa-map-marker-alt"></i> UBS Group
-                        of Companies Office No. 5,6,1 1,12, First Floor, Sardar
-                        Arcade, G-11 Markaz, Islamabad
-                      </p>
-                      <Button
-                        className="btn-icon btn-round"
-                        color="info"
-                        href="https://www.facebook.com/UBSBusinessGroup/"
-                        target="_blank"
-                      >
-                        <i className="fab fa-facebook-square"></i>
-                      </Button>
-                      <Button
-                        className="btn btn-round float-right"
-                        color="info"
-                        href="mailto:ibadsheryar@hotmail.com"
-                        target="_blank"
-                      >
-                        Send an Email
-                      </Button>
-                    </CardBody>
-                  </Card>
-                </Col>
-                <Col md="3"></Col>
-              </Row>
-            </div>
+            <Row>
+              <Col md="3"></Col>
+              <Col md="6">
+                <Card>
+                  <CardBody className="text-left">
+                    <p className="description text-left font-weight-normal">
+                      <i className="pr-1 fas fa-phone-square"></i> +923004548461
+                      <br></br>
+                      <i className="fas fa-fax"></i> 0092 51 2363409
+                      <br></br>
+                      <i className="pr-1 fas fa-envelope"></i>{" "}
+                      ibadsheryar@hotmail.com
+                      <br></br>
+                      <i className="pr-1 fas fa-map-marker-alt"></i> UBS Group
+                      of Companies Office No. 5,6,1 1,12, First Floor, Sardar
+                      Arcade, G-11 Markaz, Islamabad
+                    </p>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="https://www.facebook.com/UBSBusinessGroup/"
+                      target="_blank"
+                    >
+                      <i className="fab fa-facebook-square"></i>
+                    </Button>
+                    <Button
+                      className="btn btn-round float-right"
+                      color="info"
+                      href="mailto:ibadsheryar@hotmail.com"
+                      target="_blank"
+                    >
+                      Send an Email
+                    </Button>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col md="3"></Col>
+            </Row>
           </Container>
         </div>
         <TransparentFooter />
