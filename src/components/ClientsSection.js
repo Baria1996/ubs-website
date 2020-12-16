f474e9a4d609d8c6de6ebd48ddd12ecef584e64c
baria aziz <bariaziz1996@gmail.com>
2020-12-16 17:50:12 +0500

    add client logos, update clients section

--- src/components/ClientsSection.js
+++ src/components/ClientsSection.js
@@ -6,58 +6,205 @@ import { Container, Row, Col } from "reactstrap";
 function ClientsSection() {
   return (
     <>
-      <div className="wrapper">
-        <div className="section text-center">
-          <Container>
-            {/* <h2 className="title">Our work</h2> */}
-            <div className="team ">
-              <Row>
-                <Col md="12">
-                  <img
-                    alt="..."
-                    className="img-fluid"
-                    src={require("assets/img/clients/erra.png")}
-                  ></img>
-                  <img
-                    alt="..."
-                    className="img-fluid"
-                    src={require("assets/img/clients/green1.png")}
-                  ></img>
-                  <img
-                    alt="..."
-                    className="img-fluid"
-                    src={require("assets/img/clients/unicef.png")}
-                  ></img>
-                  <img
-                    alt="..."
-                    className="img-fluid"
-                    src={require("assets/img/clients/green2.png")}
-                  ></img>
-                  <img
-                    alt="..."
-                    className="img-fluid"
-                    src={require("assets/img/clients/mpcl.png")}
-                  ></img>
-                  <img
-                    alt="..."
-                    className="img-fluid mt-4"
-                    src={require("assets/img/clients/ici.png")}
-                  ></img>
-                  <img
-                    alt="..."
-                    className="img-fluid mt-4"
-                    src={require("assets/img/clients/hydro.png")}
-                  ></img>
-                  <img
-                    alt="..."
-                    className="img-fluid mt-4"
-                    src={require("assets/img/clients/read.png")}
-                  ></img>
-                </Col>
-              </Row>
-            </div>
-          </Container>
-        </div>
+      <div className="section text-center">
+        <Container>
+          <div className="team ">
+            <Row>
+              <Col md="12">
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture7.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture8.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture9.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/zealcon.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture11.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture12.jpg")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture13.jpg")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture14.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture15.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture16.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture17.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture18.jpg")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture19.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture29.jpg")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture21.jpg")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture22.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture23.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture24.jpg")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture25.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture26.jpg")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture27.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture28.jpg")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture20.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture30.jpg")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture31.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture32.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture33.jpg")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture34.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture35.jpg")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture36.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture37.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture38.jpg")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture39.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture40.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture41.jpg")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture42.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/read.png")}
+                  alt="..."
+                />
+                <img
+                  className="img zoom-50 m-4"
+                  src={require("assets/img/clients/Picture43.png")}
+                  alt="..."
+                />
+              </Col>
+            </Row>
+          </div>
+        </Container>
       </div>
     </>
   );
