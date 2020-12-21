e709bca68a5597a4aff450e8da1d4ed22abc2051
baria aziz <bariaziz1996@gmail.com>
2020-12-21 17:12:30 +0500

    add page: our solutions

---
+++ src/components/ProductCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+// reactstrap components
+import { Row, Col, Card, CardBody } from "reactstrap";
+
+function ProductCard(props) {
+  return (
+    <>
+      <Card>
+        <CardBody>
+          <Row>
+            <Col md="5">
+              <img alt="..." className=" img-fluid m-0" src={props.image}></img>
+            </Col>
+            <Col md="7">
+              <p className="description font-weight-normal text-left">
+                {props.description}
+              </p>
+            </Col>
+          </Row>
+        </CardBody>
+      </Card>
+    </>
+  );
+}
+
+export default ProductCard;
---
+++ src/views/OurSolutions.js
@@ -0,0 +1,132 @@
+import React from "react";
+import ProductCard from "../components/ProductCard";
+
+// reactstrap components
+import { Container, Row, Col } from "reactstrap";
+
+// core components
+import PrimaryNavbar from "components/Navbars/PrimaryNavbar.js";
+import DarkFooter from "components/Footers/DarkFooter.js";
+
+const items = [
+  {
+    image: require("assets/img/products/SANDWICH PANELS (3).jpeg"),
+    description: [
+      "EPS Sandwich panel is composite two layers pre-painted steel sheets and (EPS) polystyrene hard foams. Used as roof, wall and cold room materials. EPS foam sandwich panel also be called Foam or Polystyrene Sandwich panel widely use for Prefab Houses, Commercial Centers, Warehouses, Farm houses, Industrial partitioning, Multi Storey Buildings, Hospitals, Porta Cabins etc",
+      <br></br>,
+      <br></br>,
+      "Thickness : 50mm, 75mm, 100mm",
+      <br></br>,
+      "Width : 955 mm",
+      <br></br>,
+      "Length : As per client required",
+      <br></br>,
+      "Color Roof : Red/Blue",
+    ],
+  },
+  {
+    image: require("assets/img/products/SANDWICH PANELS (13).jpeg"),
+    description: [
+      "Color steel sandwich panel roof is a common product in the current building materials, not only can be very good flame retardant and environmentally friendly and efficient.  sandwich panels have two layers of metal panels and middle layer of polymer insulation core pressed. With easy installation, light weight, environmentally friendly and efficient features. Moreover, the closed-cell molecular structure used in the filling system can prevent condensation of water vapor.",
+      <br></br>,
+      <br></br>,
+      "Thickness : 50mm, 75mm, 100mm",
+      <br></br>,
+      "Width : 955 mm",
+      <br></br>,
+      "Length : As per client required",
+      <br></br>,
+      "Color Roof : Blue",
+    ],
+  },
+  {
+    image: require("assets/img/products/SANDWICH PANELS (5).jpeg"),
+    description: [
+      "EPS Sandwich Wall Panel (Plain & Bearing line)",
+      <br></br>,
+      <br></br>,
+      "Thickness : 50mm, 75mm & 100mm",
+      <br></br>,
+      "Width : 955 mm & 1155mm",
+      <br></br>,
+      "Length : As per client required",
+      <br></br>,
+      "Color : White",
+      <br></br>,
+      "Density of Thermopore : 15 kg/m3 - 22 kg/m3",
+      <br></br>,
+      "Delivery Time :  According to Order Quantity",
+    ],
+  },
+  {
+    image: require("assets/img/products/shed.jpg"),
+    description: [
+      "Designing , Fabrication & Erection ( a complete team of Experts ) Faster Production , Precise Fabrication, Quick  Erection & Construction. 5000-10,000 Sqft erected within 15-20 working days.",
+    ],
+  },
+  {
+    image: require("assets/img/products/setup.jpg"),
+    description: [
+      "Our Team completed this exceptional office on turnkey basis in record time! From design to complete office set up. (civil , fabrication , erection and electrical wiring fitting). Call us today to have an  for your Office, Residence and Warehouses by our experienced team.",
+    ],
+  },
+  {
+    image: require("assets/img/products/office.jpg"),
+    description: [
+      "Placing supervisors and management close to operations in a warehouse can increase productivity and help improve communication. PortaFab Modular Building Systems offer many options for inplant warehouse offices. Offices can be built on top of mezzanines to increase storage while improving visibility.",
+      <br></br>,
+      <br></br>,
+      "By choosing a modular building system over traditional construction, one minimizes the disruption to the warehouse during construction and offices can be re-purposed, moved or renovated in the future.",
+    ],
+  },
+];
+
+function OurSolutions() {
+  React.useEffect(() => {
+    document.body.classList.add("profile-page");
+    document.body.classList.add("sidebar-collapse");
+    document.documentElement.classList.remove("nav-open");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove("profile-page");
+      document.body.classList.remove("sidebar-collapse");
+    };
+  }, []);
+  return (
+    <>
+      <PrimaryNavbar />
+      <div className="wrapper">
+        <div className="section">
+          <Container>
+            <h2 className="title">Our solutions</h2>
+            <p className="text-muted text-center font-weight-normal">
+              We invite you to explore our variety of cost efficient and quality
+              designed structures, built for a lifetime to endure severe weather
+              conditions. Select a standard building type or customize your
+              structure dependent upon your requirements and visionary style!
+            </p>
+            <div className="section">
+              <Row>
+                {items.map((item, index) => {
+                  return (
+                    <Col md="12" key={index}>
+                      <ProductCard
+                        heading={item.heading}
+                        image={item.image}
+                        description={item.description}
+                      />
+                    </Col>
+                  );
+                })}
+              </Row>
+            </div>
+          </Container>
+        </div>
+        <DarkFooter />
+      </div>
+    </>
+  );
+}
+
+export default OurSolutions;
