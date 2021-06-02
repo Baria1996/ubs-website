9030f15404a149f5f92069a206d938e87a46931f
baria aziz <bariaziz1996@gmail.com>
2021-06-02 16:05:39 +0500

    update the 'our solutions' index section

--- src/views/index-sections/OurSolutions.js
+++ src/views/index-sections/OurSolutions.js
@@ -9,64 +9,63 @@ function OurSolutions() {
       <div className="wrapper">
         <div className="section section-team text-center">
           <Container>
-            <h2 className="title">Our solutions</h2>
+            <h2 className="title">Sandwich Panels</h2>
             <div className="team">
               <Row>
                 <Col md="4">
                   <div className="team-player mb-4 pb-4">
                     <img
                       alt="..."
-                      className="img-fluid img-raised rounded"
-                      src={require("assets/img/products/SANDWICH PANELS (13).jpeg")}
-                    ></img>
-                    <h4 className="title text-black mt-0">
-                      Roof Sandwich Panel
-                    </h4>
-                    <p className="description font-weight-normal">
-                      Color steel sandwich panel roof is a common product in the
-                      current building materials, with light weight, efficient
-                      and environmentally friendly features.
-                    </p>
-                  </div>
-                </Col>
-                <Col md="4">
-                  <div className="team-player mb-4 pb-4">
-                    <img
-                      alt="..."
-                      className="img-fluid img-raised rounded"
-                      src={require("assets/img/products/SANDWICH PANELS (3).jpeg")}
-                    ></img>
-                    <h4 className="title text-black mt-0">
-                      EPS Sandwich panel
-                    </h4>
-                    <p className="description font-weight-normal">
-                      EPS Sandwich panel is composite two layers pre-painted
-                      steel sheets and (EPS) polystyrene hard foams. Used as
-                      roof, wall and cold room materials.
-                    </p>
-                  </div>
-                </Col>
-                <Col md="4">
-                  <div className="team-player mb-4 pb-4">
-                    <img
-                      alt="..."
-                      className="img-fluid img-raised rounded"
+                      className="img-raised rounded img-fit-index"
                       src={require("assets/img/products/SANDWICH PANELS (5).jpeg")}
                     ></img>
                     <h4 className="title text-black mt-0">
-                      Wall Sandwich panel
+                      EPS Sandwich Panel
                     </h4>
-                    <p className="description font-weight-normal">
+                    {/* <p className="description font-weight-normal">
+                      Color steel sandwich panel roof is a common product in the
+                      current building materials, with light weight, efficient
+                      and environmentally friendly features.
+                    </p> */}
+                  </div>
+                </Col>
+                <Col md="4">
+                  <div className="team-player mb-4 pb-4">
+                    <img
+                      alt="..."
+                      className="img-raised rounded img-fit-index"
+                      src={require("assets/img/products/PU2.jpg")}
+                    ></img>
+                    <h4 className="title text-black mt-0">
+                      PU/PIR Sandwich panel
+                    </h4>
+                    {/* <p className="description font-weight-normal">
+                      EPS Sandwich panel is composite two layers pre-painted
+                      steel sheets and (EPS) polystyrene hard foams. Used as
+                      roof, wall and cold room materials.
+                    </p> */}
+                  </div>
+                </Col>
+                <Col md="4">
+                  <div className="team-player mb-4 pb-4">
+                    <img
+                      alt="..."
+                      className="img-raised rounded img-fit-index"
+                      src={require("assets/img/products/rockwool.jpg")}
+                    ></img>
+                    <h4 className="title text-black mt-0">
+                      Rockwool Sandwich panel
+                    </h4>
+                    {/* <p className="description font-weight-normal">
                       EPS Sandwich panel is kind of board used for enclosing
                       structures, which has good heat insulation and higher
                       intensity, and has Light Guage Weight
-                    </p>
+                    </p> */}
                   </div>
                 </Col>
               </Row>
               <Row>
                 <Col md="12">
-                  <div className="space-50"></div>
                   <Button className="" color="info" href="our-solutions">
                     View all products
                   </Button>
