6dc1a4fd5caaaab9b237ce095d032884b6bd72e3
baria aziz <bariaziz1996@gmail.com>
2021-06-03 15:03:09 +0500

    add carousel to main header, replace images

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -2,55 +2,130 @@
 import React from "react";
 
 // reactstrap components
-import { Container } from "reactstrap";
-// core components
+import { Carousel, CarouselItem, CarouselIndicators } from "reactstrap";
+
+const items = [
+  {
+    src: require("assets/img/main-bg-1.jpg"),
+    altText: "...",
+    caption: "",
+  },
+  {
+    src: require("assets/img/main-bg-2.jpg"),
+    altText: "...",
+    caption: "",
+  },
+  {
+    src: require("assets/img/main-bg-3.jpg"),
+    altText: "...",
+    caption: "",
+  },
+  {
+    src: require("assets/img/main-bg-4.jpg"),
+    altText: "...",
+    caption: "",
+  },
+  {
+    src: require("assets/img/main-bg-5.jpg"),
+    altText: "...",
+    caption: "",
+  },
+  {
+    src: require("assets/img/main-bg-6.jpg"),
+    altText: "...",
+    caption: "",
+  },
+];
 
 function IndexHeader() {
-  let pageHeader = React.createRef();
-
-  React.useEffect(() => {
-    if (window.innerWidth > 991) {
-      const updateScroll = () => {
-        let windowScrollTop = window.pageYOffset / 3;
-        pageHeader.current.style.transform =
-          "translate3d(0," + windowScrollTop + "px,0)";
-      };
-      window.addEventListener("scroll", updateScroll);
-      return function cleanup() {
-        window.removeEventListener("scroll", updateScroll);
-      };
-    }
-  });
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const [animating, setAnimating] = React.useState(false);
+  const onExiting = () => {
+    setAnimating(true);
+  };
+  const onExited = () => {
+    setAnimating(false);
+  };
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
 
   return (
     <>
-      <div className="page-header" filter-color="orange">
-        <div
-          className="page-header-image"
-          style={{
-            backgroundImage:
-              "linear-gradient(#000000aa , #ffffff00), url(" +
-              require("assets/img/main-bg.jpg") +
-              ")",
-          }}
-          ref={pageHeader}
-        ></div>
-        <Container>
-          <div className="content-center brand">
-            <img
-              className="mt-4 pt-4"
-              src={require("assets/img/logo.png")}
-              alt="..."
-            />
+      <div className="page-header">
+        <div className="page-title d-flex justify-content-center">
+          <div className="page-title-inner">
+            <div className="d-flex justify-content-center">
+              <img src={require("assets/img/logo.png")} alt="..." />
+            </div>
             <h1 className="text-primary font-weight-bold mt-4">
               <em>Group of Companies</em>
             </h1>
-            <h3>The Construction Revolution</h3>
+            <h3 className="font-weight-normal">The Construction Revolution</h3>
           </div>
-          <h6 className="category category-absolute">
-            Delivery all accross Pakistan.
-          </h6>
-        </Container>
+        </div>
+        <Carousel
+          activeIndex={activeIndex}
+          next={next}
+          previous={previous}
+          ride="carousel"
+          className="main-carousel"
+        >
+          <CarouselIndicators
+            items={items}
+            activeIndex={activeIndex}
+            onClickHandler={goToIndex}
+          />
+          {items.map((item) => {
+            return (
+              <CarouselItem
+                onExiting={onExiting}
+                onExited={onExited}
+                key={item.src}
+              >
+                <img src={item.src} alt={item.altText} />
+                <div className="carousel-caption d-none d-md-block">
+                  <h5 className="font-weight-bold shadow-sm">{item.caption}</h5>
+                </div>
+              </CarouselItem>
+            );
+          })}
+          <a
+            className="carousel-control-prev"
+            data-slide="prev"
+            href="#pablo"
+            onClick={(e) => {
+              e.preventDefault();
+              previous();
+            }}
+            role="button"
+          >
+            <i className="fas fa-chevron-left font-weight-bold shadow-lg"></i>
+          </a>
+          <a
+            className="carousel-control-next"
+            data-slide="next"
+            href="#pablo"
+            onClick={(e) => {
+              e.preventDefault();
+              next();
+            }}
+            role="button"
+          >
+            <i className="fas fa-chevron-right font-weight-bold shadow-lg"></i>
+          </a>
+        </Carousel>
       </div>
     </>
   );
